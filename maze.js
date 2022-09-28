f1fde829c87f2ed42eec9fbb57816eeabadc8573
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2022-09-28 11:01:41 +0300

    Done

--- maze.js
+++ maze.js
@@ -1,18 +1,20 @@
 let lines = prompt("Lines: ", '');
 let columns = prompt("Columns: ", '');
 
-let matrix = [["#", "S", "#", "E", ".", "#", "#"],
-              ["#", ".", "#", "#", "#", ".", "#"],
-              ["#", ".", "#", ".", ".", ".", "#"],
-              ["#", ".", ".", ".", ",", "#", "#"],
-              ["#", "#", ".", "#", "#", "#", "#"]];
+let matrix = [["#", "S", "#", "#", "#"],
+              ["#", ".", "#", ".", "E"],
+              ["#", ".", "#", ".", "#"],
+              ["#", ".", ".", ".", "#"],
+              ["#", "#", "#", "#", "#"]];
 
 let startPoint = readPoint(lines, columns, 'S', '#');
 let endPoint = readPoint(lines, columns, 'E', '.');
 
 let queue = [];
 
-alert ( searchWay(lines-1, columns-1, startPoint, endPoint) ? "Yes" : "No");
+alert(searchWay(lines - 1, columns - 1, startPoint, endPoint) ? "Yes" : "No");
+console.log(matrix);
+alert(outWay(matrix, endPoint));
 
 function readPoint(lines, columns, ind, symbol) {
 
@@ -23,6 +25,7 @@ function readPoint(lines, columns, ind, symbol) {
 
             matrix[i][j] = symbol;
             return [i, j];
+        
         }
     }
 }
@@ -36,35 +39,65 @@ function searchWay(lines, columns, start, end) {
         let j = point[1];
 
         if(i === end[0] && j === end[1]) return true;
-        
-        if(checkBorder(0, i-1, i-1, j)) { //граница низ
-            setValues(i - 1, j);
+
+        if (checkPoint(0, i - 1, i - 1, j)) { //поинт низ
+            setValues(i - 1, j, 'D');
         }
 
-        if(checkBorder(+i + 1, lines, +i + 1, j)) { //граница верх
-            setValues(+i + 1, j);
+        if(checkPoint(+i + 1, lines, +i + 1, j)) { //поинт верх
+            setValues(+i + 1, j, 'U');
         }
 
-        if(checkBorder(0, j - 1, i, j - 1)) { //граница лево
-            setValues(i, j - 1);
+        if(checkPoint(0, j - 1, i, j - 1)) { //поинт лево
+            setValues(i, j - 1, 'R');
         }
 
-        if(checkBorder(+j + 1, columns, i, +j + 1)) { //граница право
-            setValues(i, +j + 1);
+        if(checkPoint(+j + 1, columns, i, +j + 1)) { //поинт право
+            setValues(i, +j + 1, 'L');
         }
     }
 
     return false;
 }
 
-function checkBorder(index, number, i, j) {
+function checkPoint(index, number, i, j) {
     if(index <= number) 
         if(matrix[i][j] === '.') return true;
 
     return false;
 }
 
-function setValues(i, j) {
+function setValues(i, j, code) {
     queue.push([i, j]);
-    matrix[i][j] = '#';
+    matrix[i][j] = code;
+}
+
+function outWay(matrix, endPoint) {
+    let way = [];
+
+    let i = endPoint[0],
+        j = endPoint[1];
+    
+    while (matrix[i][j] !== '#') {
+
+        if (matrix[i][j] === 'U') {
+            way.push(' d');
+            i--; continue;}
+
+        if (matrix[i][j] === 'D') {
+            way.push(' u');
+            i++; continue;}
+        
+        if (matrix[i][j] === 'R') {
+            way.push(' l');
+            j++; continue;
+        }
+        
+        if (matrix[i][j] === 'L') {
+            way.push(' r');
+            j--;  continue;
+        }
+    }
+
+    return way.reverse();
 }
